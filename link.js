a10abea728445e92b9fe4483afc5a429802bb1b0
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2022-08-31 12:06:31 -0300

    responsive stud adjust

--- link.js
+++ link.js
@@ -16,19 +16,3 @@ function busca(input_field, div) {
     }
 }
 
-function buttonUpper() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
-function definirBotao() {
-    if (window.scrollY === 0) {
-        document.querySelector('.button-upper').style.display = 'none';
-    } else {
-        document.querySelector('.button-upper').style.display = 'block';
-    }
-}
-
-window.addEventListener('scroll', definirBotao);
